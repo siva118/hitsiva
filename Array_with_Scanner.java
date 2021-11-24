1d6ca24d378e0be294af0b2bd01797a1b43a1870
siva118 <81425061+siva118@users.noreply.github.com>
2021-11-24 19:01:00 +0530

    Add files via upload

---
+++ Array_with_Scanner.java
@@ -0,0 +1,26 @@
+package com.technoelevate.basicprograms;
+
+
+
+public class Array_with_Scanner {
+	public static int a;
+	public static void main(String[] args) {
+		
+		Array_with_Scanner s=new Array_with_Scanner();
+
+				s.siva(10);
+		System.out.println(s.mark());
+		
+	}
+	public void siva(int n) {
+		
+		a=n;
+	}
+public int mark() {
+		
+		return a;
+	}
+	
+	
+
+}
